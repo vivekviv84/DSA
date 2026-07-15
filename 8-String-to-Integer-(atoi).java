b8cad68ba2cabbf7133d4470e9e0f4f72117bd55
Vivek Kumar Dubey <vivekviv84@gmail.com>
2026-07-15 12:09:36 +0530

    [8] [Time Beats: 100.00%] [Memory Beats: 60.08%] - LeetPush

---
+++ 8-String-to-Integer-(atoi).java
@@ -0,0 +1,36 @@
+class Solution {
+    public int myAtoi(String s) {
+
+        s = s.trim();
+
+        if (s.length() == 0)
+            return 0;
+
+        int sign = 1;
+        int i = 0;
+        int ans = 0;
+
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+
+            int digit = s.charAt(i) - '0';
+
+            if (ans > Integer.MAX_VALUE / 10 ||
+               (ans == Integer.MAX_VALUE / 10 && digit >= (sign == 1 ?7:8))) {
+
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            ans = ans * 10 + digit;
+            i++;
+        }
+
+        return sign * ans;
+    }
+}
