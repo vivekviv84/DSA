c7ea883f6459bde4fad382ff882f2579cd63c44c
Vivek Kumar Dubey <vivekviv84@gmail.com>
2026-07-22 01:27:40 +0530

    [234] [Time Beats: 99.83%] [Memory Beats: 55.57%] - LeetPush

---
+++ 234-Palindrome-Linked-List.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second != null) {
+            if (first.val != second.val) {
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        return true; 
+    }
+}
