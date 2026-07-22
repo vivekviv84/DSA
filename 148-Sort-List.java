b90ad658bb3452ccd12b503fda2fc4288c285c8f
Vivek Kumar Dubey <vivekviv84@gmail.com>
2026-07-22 17:58:43 +0530

    [148] [Time Beats: 98.78%] [Memory Beats: 30.88%] - LeetPush

--- 148-Sort-List.java
+++ 148-Sort-List.java
@@ -10,25 +10,54 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-
         if(head == null || head.next == null){
             return head;
         }
 
-        ArrayList<Integer> value = new ArrayList<>();
-        
-        ListNode curr = head;
-        while(curr != null){
-            value.add(curr.val);
-            curr = curr.next;
-        }
-        Collections.sort(value);
-        ListNode prev = head;
+        ListNode mid = findmiddle(head);
+        ListNode righthead = mid.next;
 
-        for(int val: value){
-            prev.val = val;
-            prev = prev.next;
+        mid.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(righthead);
+
+        return merge(left,right);
+    }
+
+    public ListNode findmiddle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return head;
+        return slow;
+    }
+
+    public ListNode  merge(ListNode left,ListNode right){
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        while( left != null && right!= null){
+            if(left.val <= right.val){
+                tail.next = left;
+                left = left.next;
+            }
+            else{
+                tail.next = right;
+                right =right.next;
+            }
+
+            tail = tail.next;
+        }
+        if(left != null){
+            tail.next = left;
+        }
+
+        if(right != null){
+            tail.next = right;
+        }
+        return dummy.next;
     }
 }
